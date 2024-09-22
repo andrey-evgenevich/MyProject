1da7c42cb5342539de2d7ad033ed026e91a0a9b3
andrey sokolov <andrey.evgenevich.s@gmail.com>
2024-09-22 19:03:49 +0800

    Обновлены модели

--- catalog/models.py
+++ catalog/models.py
@@ -5,14 +5,14 @@ class Product(models.Model):
     name = models.CharField(max_length=50,
                             verbose_name='Наименование продукта',
                             help_text='Введите наименование продукта')
-    description = models.TextField(verbose_name='Описание продукта')
+    description = models.TextField(verbose_name='Описание продукта', blank=True, null=True)
     photo = models.ImageField(upload_to='product/photo',
                               blank=True,
                               null=True)
     category = models.CharField(max_length=100,
-                                verbose_name='Категория продукта')
-    price = models.IntegerField(verbose_name='Наименование продукта')
-    date_create = models.DateTimeField(verbose_name='Дата создания')
+                                verbose_name='Категория продукта', blank=True, null=True)
+    price = models.IntegerField(verbose_name='Цена продукта', blank=True, null=True)
+    date_create = models.DateTimeField(verbose_name='Дата создания', blank=True, null=True)
     date_change = models.DateTimeField(verbose_name='Дата последнего изменения')
     manufactured_at = models.DateTimeField(verbose_name='Дата производства продукта',
                                            blank=True,
