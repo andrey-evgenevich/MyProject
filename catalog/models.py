217abfab70f4bccb7e23cceb9f98f7ffe62b42eb
andrey sokolov <andrey.evgenevich.s@gmail.com>
2024-09-22 16:43:19 +0800

    Описаны модели

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,37 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    name = models.CharField(max_length=50,
+                            verbose_name='Наименование продукта',
+                            help_text='Введите наименование продукта')
+    description = models.TextField(verbose_name='Описание продукта')
+    photo = models.ImageField(upload_to='product/photo',
+                              blank=True,
+                              null=True)
+    category = models.CharField(max_length=100,
+                                verbose_name='Категория продукта')
+    price = models.IntegerField(verbose_name='Наименование продукта')
+    date_create = models.DateTimeField(verbose_name='Дата создания')
+    date_change = models.DateTimeField(verbose_name='Дата последнего изменения')
+
+    class Meta:
+        verbose_name = "Продукт"
+        ordering = ["name", "category"]
+
+    def __str__(self):
+        return self.name
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=50,
+                            verbose_name='Наименование категории',
+                            help_text='Введите наименование категории')
+    description = models.TextField(verbose_name='Описание категории')
+
+    class Meta:
+        verbose_name = "Категория"
+        ordering = ["name"]
+
+    def __str__(self):
+        return self.name
