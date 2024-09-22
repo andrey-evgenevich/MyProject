ea7acddcf9f1eed461782532bdaedee7cdd5f011
andrey sokolov <andrey.evgenevich.s@gmail.com>
2024-09-22 17:35:49 +0800

    Добавлены verbose_name_plural

--- catalog/models.py
+++ catalog/models.py
@@ -20,6 +20,7 @@ class Product(models.Model):
 
     class Meta:
         verbose_name = "Продукт"
+        verbose_name_plural = "Продукты"
         ordering = ["name", "category"]
 
     def __str__(self):
@@ -34,6 +35,7 @@ class Category(models.Model):
 
     class Meta:
         verbose_name = "Категория"
+        verbose_name_plural = "Категории"
         ordering = ["name"]
 
     def __str__(self):
