27058825303cf51eefada22242f5ad17fc6a9135
andrey sokolov <andrey.evgenevich.s@gmail.com>
2024-10-06 13:47:24 +0800

    Добавлены изображения

--- catalog/views.py
+++ catalog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from catalog.models import Product
 
 
@@ -9,7 +9,7 @@ def products_list(request):
 
 
 def products_detail(request, pk):
-    product = Product.objects.get(pk=pk)
+    product = get_object_or_404(Product, pk=pk)
     context = {"product": product}
     return render(request, "products_detail.html", context)
 
