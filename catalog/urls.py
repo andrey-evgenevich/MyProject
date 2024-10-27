0ccbdfdd178c7a82b8ea332cc1421ab17b0b17d7
andrey sokolov <andrey.evgenevich.s@gmail.com>
2024-10-27 12:59:50 +0800

    home и contacts переведены на CBV

--- catalog/urls.py
+++ catalog/urls.py
@@ -6,9 +6,9 @@ from catalog.views import HomeTemplateView, ContactsTemplateView, ProductDetailV
 app_name = CatalogConfig.name
 
 urlpatterns = [
-    path("", HomeTemplateView.as_view, name="home"),
+    path("", HomeTemplateView.as_view(), name="home"),
     path("products/", ProductListView.as_view(), name="products_list"),
-    path("contacts/", ContactsTemplateView.as_view, name="contacts"),
+    path("contacts/", ContactsTemplateView.as_view(), name="contacts"),
     path("products/<int:pk>", ProductDetailView.as_view(), name="products_detail"),
     path("catalog/create", ProductCreateView.as_view(), name="products_create"),
     path("catalog/<int:pk>/update", ProductUpdateView.as_view(), name="products_update"),
